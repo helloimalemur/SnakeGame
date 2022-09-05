b89d7336de1305dc1a5abb1a196ed3a9700dbd21
James Koonts <jkoontsiii@gmail.com>
2022-09-05 14:32:19 -0400

    .

--- src/main/java/net/koonts/GamePanel.java
+++ src/main/java/net/koonts/GamePanel.java
@@ -22,6 +22,7 @@ public class GamePanel extends JPanel implements ActionListener {
     boolean running = false;
     Timer timer;
     Random random;
+
     GamePanel() {
         random = new Random();
         this.setPreferredSize(new Dimension(600,600));
@@ -33,28 +34,60 @@ public class GamePanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(running) {
+            move();
+            checkApple();
+            checkCollision();
+        } else {
+            timer.stop();
 
+        }
+        repaint();
 
     }
 
     public void startGame() {
+        applesEaten = 0;
+        bodyParts = 6;
         newApple();
         running = true;
         timer = new Timer(DELAY, this);
         timer.start();
     }
+    @Override //https://www.bogotobogo.com/Java/tutorials/javagraphics3.php
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
     }
     public void draw(Graphics g) {
+        if (running) {
+            for (int i = 0; i < (SCREEN_HEIGHT / UNIT_SIZE); i++) {
+                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+            }
+            g.setColor(Color.red);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
-        for(int i=0;i<(SCREEN_HEIGHT/UNIT_SIZE);i++) {
-            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
-            g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) {
+                    g.setColor(Color.green);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                } else {
+                    g.setColor(Color.blue);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+
+            }
+        } else {
+            gameOver(g);
         }
+
         g.setColor(Color.red);
-        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        g.setFont(new Font("Ink Free", Font.BOLD, 40));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + applesEaten))/2, g.getFont().getSize());
+
+
     }
     public void newApple() {
         //https://youtu.be/bI6e6qjJ8JQ?t=1194
@@ -65,27 +98,106 @@ public class GamePanel extends JPanel implements ActionListener {
         appleY = random.nextInt((int) (SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
         System.out.println(appleX);
         System.out.println(appleY);
-
-        int a = (SCREEN_WIDTH/UNIT_SIZE)*UNIT_SIZE;
-        int b = (SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE;
-        int c = (int) (SCREEN_WIDTH/UNIT_SIZE)*UNIT_SIZE;
-        int d = (int)(SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-
-
     }
-    public void move() {}
+    public void move() {
+        for(int i=bodyParts;i>0;i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+        }
+    }
 
-    public void checkApple() {}
-    public void checkCollision() {}
-    public void gameOver(Graphics g) {}
+    public void checkApple() {
+        //check if head touches apple
+        if (x[0] == appleX && y[0] == appleY) {
+            applesEaten += 1;
+            System.out.println("Apples Eaten: " + applesEaten);
+            newApple();
+            bodyParts += 1;
+        }
+    }
+    public void checkCollision() {
+        //check if head collided with body
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+
+            }
+        }
+        //check if head touches left border
+        if (x[0] < 0) {
+            running = false;
+        }
+        //check if head touches right border
+        if (x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+        //check if head touches top border
+        if (y[0] < 0) {
+            running = false;
+        }
+        //check if head touches bottom border
+        if (y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+    }
+
+    public void gameOver(Graphics g) {
+        //game over text
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD, 75));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+        g.setColor(Color.red);
+
+        //ending score
+        g.setFont(new Font("Ink Free", Font.BOLD, 40));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics2.stringWidth("Score: " + applesEaten))/2, g.getFont().getSize());
+    }
 
     public class myKeyAdapter extends KeyAdapter implements KeyListener {
         @Override
         public void keyPressed(KeyEvent d) {
+            //direction up
+            if (d.getKeyChar() == 'w') {
+                if (direction != 'D') {}
+                direction='U';
+            }
+            //direction
+            if (d.getKeyChar() == 'a') {
+                if (direction != 'R') {}
+                direction = 'L';
+            }
+            if (d.getKeyChar() == 's') {
+                if (direction != 'U') {}
+                direction = 'D';
+            }
+            if (d.getKeyChar() == 'd') {
+                if (direction != 'L') {}
+                direction = 'R';
+            }
+
+
+
+            if (d.getKeyChar() == 'r') {
+                x[0] = random.nextInt((int) (SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+                y[0] = random.nextInt((int) (SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+                startGame();
+            }
             if (d.getKeyChar() == 'z') {
                 System.exit(0);
             }
